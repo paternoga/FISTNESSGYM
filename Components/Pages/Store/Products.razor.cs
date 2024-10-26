5d04824fb6e14599fd0250b6917c2be1c27e50cf
Bartosz Olejnik <bartek_01@o2.pl>
2024-10-26 16:43:55 +0200

    added products, orders, polish version

---
+++ Components/Pages/Store/Products.razor.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Radzen;
+using Radzen.Blazor;
+
+namespace FISTNESSGYM.Components.Pages.Store
+{
+    public partial class Products
+    {
+        [Inject]
+        protected IJSRuntime JSRuntime { get; set; }
+
+        [Inject]
+        protected NavigationManager NavigationManager { get; set; }
+
+        [Inject]
+        protected DialogService DialogService { get; set; }
+
+        [Inject]
+        protected TooltipService TooltipService { get; set; }
+
+        [Inject]
+        protected ContextMenuService ContextMenuService { get; set; }
+
+        [Inject]
+        protected NotificationService NotificationService { get; set; }
+
+        [Inject]
+        public databaseService databaseService { get; set; }
+
+        protected IEnumerable<FISTNESSGYM.Models.database.Product> products;
+
+        protected RadzenDataGrid<FISTNESSGYM.Models.database.Product> grid0;
+
+        [Inject]
+        protected SecurityService Security { get; set; }
+        protected override async Task OnInitializedAsync()
+        {
+            products = await databaseService.GetProducts();
+        }
+
+        protected async Task AddButtonClick(MouseEventArgs args)
+        {
+            await DialogService.OpenAsync<AddProduct>("Add Product", null);
+            await grid0.Reload();
+        }
+
+        protected async Task EditRow(FISTNESSGYM.Models.database.Product args)
+        {
+            await DialogService.OpenAsync<EditProduct>("Edit Product", new Dictionary<string, object> { {"Id", args.Id} });
+        }
+
+        protected async Task GridDeleteButtonClick(MouseEventArgs args, FISTNESSGYM.Models.database.Product product)
+        {
+            try
+            {
+                if (await DialogService.Confirm("Are you sure you want to delete this record?") == true)
+                {
+                    var deleteResult = await databaseService.DeleteProduct(product.Id);
+
+                    if (deleteResult != null)
+                    {
+                        await grid0.Reload();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                NotificationService.Notify(new NotificationMessage
+                {
+                    Severity = NotificationSeverity.Error,
+                    Summary = $"Error",
+                    Detail = $"Unable to delete Product"
+                });
+            }
+        }
+    }
+}
