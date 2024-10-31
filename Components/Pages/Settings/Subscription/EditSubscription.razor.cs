0368c4c0e3ae36bd21a6ed9f299ddfbc9bce8d74
pater <117403458+paternoga@users.noreply.github.com>
2024-10-31 17:04:46 +0100

    Added CRUD Subscriptions

---
+++ Components/Pages/Settings/Subscription/EditSubscription.razor.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Radzen;
+using Radzen.Blazor;
+
+namespace FISTNESSGYM.Components.Pages.Settings.Subscription
+{
+    public partial class EditSubscription
+    {
+        [Inject]
+        protected IJSRuntime JSRuntime { get; set; }
+
+        [Inject]
+        protected NavigationManager NavigationManager { get; set; }
+
+        [Inject]
+        protected DialogService DialogService { get; set; }
+
+        [Inject]
+        protected TooltipService TooltipService { get; set; }
+
+        [Inject]
+        protected ContextMenuService ContextMenuService { get; set; }
+
+        [Inject]
+        protected NotificationService NotificationService { get; set; }
+        [Inject]
+        public databaseService databaseService { get; set; }
+
+        [Parameter]
+        public int Id { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            subscription = await databaseService.GetSubscriptionById(Id);
+
+            aspNetUsersForUserId = await databaseService.GetAspNetUsers();
+
+            subscriptionStatusesForSubscriptionStatusId = await databaseService.GetSubscriptionStatuses();
+        }
+        protected bool errorVisible;
+        protected FISTNESSGYM.Models.database.Subscription subscription;
+
+        protected IEnumerable<FISTNESSGYM.Models.database.AspNetUser> aspNetUsersForUserId;
+
+        protected IEnumerable<FISTNESSGYM.Models.database.SubscriptionStatus> subscriptionStatusesForSubscriptionStatusId;
+
+        [Inject]
+        protected SecurityService Security { get; set; }
+
+        protected async Task FormSubmit()
+        {
+            try
+            {
+                await databaseService.UpdateSubscription(Id, subscription);
+                DialogService.Close(subscription);
+            }
+            catch (Exception ex)
+            {
+                errorVisible = true;
+            }
+        }
+
+        protected async Task CancelButtonClick(MouseEventArgs args)
+        {
+            DialogService.Close(null);
+        }
+    }
+}
---
+++ Components/Pages/Settings/Subscription/Subscriptions.razor.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Radzen;
+using Radzen.Blazor;
+
+namespace FISTNESSGYM.Components.Pages.Settings.Subscription
+{
+    public partial class Subscriptions
+    {
+        [Inject]
+        protected IJSRuntime JSRuntime { get; set; }
+
+        [Inject]
+        protected NavigationManager NavigationManager { get; set; }
+
+        [Inject]
+        protected DialogService DialogService { get; set; }
+
+        [Inject]
+        protected TooltipService TooltipService { get; set; }
+
+        [Inject]
+        protected ContextMenuService ContextMenuService { get; set; }
+
+        [Inject]
+        protected NotificationService NotificationService { get; set; }
+
+        [Inject]
+        public databaseService databaseService { get; set; }
+
+        protected IEnumerable<FISTNESSGYM.Models.database.Subscription> subscriptions;
+
+        protected RadzenDataGrid<FISTNESSGYM.Models.database.Subscription> grid0;
+
+        [Inject]
+        protected SecurityService Security { get; set; }
+        protected override async Task OnInitializedAsync()
+        {
+            subscriptions = await databaseService.GetSubscriptions(new Query { Expand = "AspNetUser,SubscriptionStatus" });
+        }
+
+        protected async Task AddButtonClick(MouseEventArgs args)
+        {
+            await DialogService.OpenAsync<AddSubscription>("Add Subscription", null);
+            await grid0.Reload();
+        }
+
+        protected async Task EditRow(FISTNESSGYM.Models.database.Subscription args)
+        {
+            await DialogService.OpenAsync<EditSubscription>("Edit Subscription", new Dictionary<string, object> { {"Id", args.Id} });
+        }
+
+        protected async Task GridDeleteButtonClick(MouseEventArgs args, FISTNESSGYM.Models.database.Subscription subscription)
+        {
+            try
+            {
+                if (await DialogService.Confirm("Are you sure you want to delete this record?") == true)
+                {
+                    var deleteResult = await databaseService.DeleteSubscription(subscription.Id);
+
+                    if (deleteResult != null)
+                    {
+                        await grid0.Reload();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                NotificationService.Notify(new NotificationMessage
+                {
+                    Severity = NotificationSeverity.Error,
+                    Summary = $"Error",
+                    Detail = $"Unable to delete Subscription"
+                });
+            }
+        }
+    }
+}
